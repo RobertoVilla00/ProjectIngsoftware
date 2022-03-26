c0fbb486ba23464b351f72380722036a0344a1b4
Stefanizzi <giovanni.stefanizzi@mail.polimi.it>
2022-03-26 16:29:34 +0100

    added Controller, defined structure of turn

--- src/main/java/Controller/TurnController.java
+++ src/main/java/Controller/TurnController.java
@@ -1,7 +1,27 @@
 package Controller;
 
-
 public class TurnController {
+
+    public void TurnInit(){
+
+        //next phase
+    }
+
+    public void AssistantCardPhase(){
+
+    }
+
+    public void ActionPhase(){
+        //student movement
+
+        //MotherNature movement (also check dominance)
+
+
+        //CloudChoose
+
+
+        //next Turn
+    }
 
 
 }
