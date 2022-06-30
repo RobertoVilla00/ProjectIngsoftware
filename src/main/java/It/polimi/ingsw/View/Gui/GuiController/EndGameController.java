43496f31e7cc8abb4a479f3f44818c1425e39f2c
GiacomoScortecci <giacomo.scortecci@mail.polimi.it>
2022-06-30 10:33:11 +0200

    fixes in gui

--- src/main/java/It/polimi/ingsw/View/Gui/GuiController/EndGameController.java
+++ src/main/java/It/polimi/ingsw/View/Gui/GuiController/EndGameController.java
@@ -1,12 +1,18 @@
 package It.polimi.ingsw.View.Gui.GuiController;
 
 import It.polimi.ingsw.Message.EndgameMessage;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import static java.lang.System.exit;
 
 public class EndGameController {
 	@FXML
 	Label endGameLabel;
+	@FXML
+	Button exitButton;
 
 	public void displayWinner(EndgameMessage endgameMessage){
 		StringBuilder str = new StringBuilder();
@@ -15,5 +21,9 @@ public class EndGameController {
 		}
 		str.append("YOU WIN!");
 		endGameLabel.setText(str.toString());
+	}
+
+	public void closeGame(Event e){
+		exit(0);
 	}
 }
